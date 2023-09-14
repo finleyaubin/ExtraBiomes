8335a6d7b0fd6c4a0265021765ddb490a72d8b3a
finley <fia5@aber.ac.uk>
2023-09-14 13:41:37 +0100

    added vanilla tags for mining speed.

--- Extrabiomes - Forge/src/main/java/net/winepicfin/extrabiomes/block/ModBlocks.java
+++ Extrabiomes - Forge/src/main/java/net/winepicfin/extrabiomes/block/ModBlocks.java
@@ -1,10 +1,9 @@
 package net.winepicfin.extrabiomes.block;
 
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,8 +17,9 @@ import java.util.function.Supplier;
 public class ModBlocks {
    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ExtraBiomes.MOD_ID);
 
-   public static final RegistryObject<Block> DENSE_CLOUD = registerBlock("dense_cloud",()->new Block(BlockBehaviour.Properties.copy(Blocks.WHITE_WOOL).sound(SoundType.WOOL)));
-   public static final RegistryObject<Block> DENSE_CLOUD_BRICK = registerBlock("dense_cloud_brick", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE_BRICKS).sound(SoundType.WOOL)));
+   public static final RegistryObject<Block> DENSE_CLOUD = registerBlock("dense_cloud",()->new Block(BlockBehaviour.Properties.copy(Blocks.WHITE_WOOL).sound(SoundType.WOOL).strength(0.3f)));
+   public static final RegistryObject<Block> DENSE_CLOUD_BRICK = registerBlock("dense_cloud_brick", () -> new Block(BlockBehaviour.Properties.copy(Blocks.STONE_BRICKS).sound(SoundType.WOOL).strength(0.5f)));
+   public static final RegistryObject<Block> NETHER_DIAMOND_ORE = registerBlock("nether_diamond_ore",()-> new DropExperienceBlock(BlockBehaviour.Properties.copy(Blocks.NETHERRACK).strength(2f).requiresCorrectToolForDrops(), UniformInt.of(3,6)));
    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block){
       RegistryObject<T> output = BLOCKS.register(name, block);
       registerBlockItem(name, output);
