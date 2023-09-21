868b2b32382e17b7336daa8a25728841ee6eab6e
finley <fia5@aber.ac.uk>
2023-09-21 17:31:00 +0100

    Forge: tiny bit of work on datagen

--- Extrabiomes - Forge/src/main/java/net/winepicfin/extrabiomes/datagen/ModBlockStateProvider.java
+++ Extrabiomes - Forge/src/main/java/net/winepicfin/extrabiomes/datagen/ModBlockStateProvider.java
@@ -1,4 +1,26 @@
 package net.winepicfin.extrabiomes.datagen;
 
-public class ModBlockStateProvider {
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+import net.winepicfin.extrabiomes.ExtraBiomes;
+import net.winepicfin.extrabiomes.block.ModBlocks;
+import org.apache.commons.compress.compressors.lz77support.LZ77Compressor;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper existingFileHelper){
+        super(output, ExtraBiomes.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels(){
+        blockWithItem(ModBlocks.NETHER_DIAMOND_ORE);
+        blockWithItem(ModBlocks.DENSE_CLOUD_BRICK);
+        blockWithItem(ModBlocks.DENSE_CLOUD);
+    }
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject){
+        simpleBlockWithItem(blockRegistryObject.get(),cubeAll(blockRegistryObject.get()));
+    }
 }
