c1497d9d86a8e27de9c9efb79e14cbcdbac92de3
Win Epic Fin <fia5@aber.ac.uk>
2023-11-05 22:17:09 +0000

    forge: Work on Puckoo Entity

--- Extrabiomes - Forge/src/main/java/net/winepicfin/extrabiomes/entity/custom/PuckooEntity.java
+++ Extrabiomes - Forge/src/main/java/net/winepicfin/extrabiomes/entity/custom/PuckooEntity.java
@@ -3,15 +3,39 @@ package net.winepicfin.extrabiomes.entity.custom;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.AgeableMob;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.goal.BreedGoal;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
+import net.minecraft.world.entity.ai.goal.PanicGoal;
+import net.minecraft.world.entity.ai.goal.TemptGoal;
 import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.level.Level;
+import net.winepicfin.extrabiomes.item.ModItems;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Random;
 
 public class PuckooEntity extends Animal {
     public PuckooEntity(EntityType<? extends Animal> p_27557_, Level p_27558_) {
         super(p_27557_, p_27558_);
     }
 
+    @Override
+    protected void registerGoals() {
+        this.goalSelector.addGoal(0, new FloatGoal(this));
+        this.goalSelector.addGoal(1, new PanicGoal(this, 1.5));
+        this.goalSelector.addGoal(2, new BreedGoal(this,1.15));
+        this.goalSelector.addGoal(3, new TemptGoal(this, 1.20, Ingredient.of(ModItems.mossy_pebble.get()), false));
+    }
+
+    public static AttributeSupplier.Builder createAttributes(){
+        return Animal.createLivingAttributes()
+                .add(Attributes.MAX_HEALTH,6)
+                .add(Attributes.MOVEMENT_SPEED,0.35);
+    }
     @Nullable
     @Override
     public AgeableMob getBreedOffspring(ServerLevel p_146743_, AgeableMob p_146744_) {
