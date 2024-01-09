78586dbf05850a0451bbd6097baf6ef3e17145fa
finley <fia5@aber.ac.uk>
2024-01-09 13:41:55 +0000

    Forge: implemented a config file which controls the biome weight of the ExtraBiomes Biomes.

--- Extrabiomes - Forge/src/main/java/net/winepicfin/extrabiomes/worldgen/biomes/ModTerrablender.java
+++ Extrabiomes - Forge/src/main/java/net/winepicfin/extrabiomes/worldgen/biomes/ModTerrablender.java
@@ -1,11 +1,12 @@
 package net.winepicfin.extrabiomes.worldgen.biomes;
 
 import net.minecraft.resources.ResourceLocation;
+import net.winepicfin.extrabiomes.Config;
 import net.winepicfin.extrabiomes.ExtraBiomes;
 import terrablender.api.Regions;
 
 public class ModTerrablender {
     public static void registerBiomes(){
-        Regions.register(new ModOverworldRegion(new ResourceLocation(ExtraBiomes.MOD_ID,"overworld"),5));
+        Regions.register(new ModOverworldRegion(new ResourceLocation(ExtraBiomes.MOD_ID,"overworld"), Config.biomeWeight));
     }
 }
