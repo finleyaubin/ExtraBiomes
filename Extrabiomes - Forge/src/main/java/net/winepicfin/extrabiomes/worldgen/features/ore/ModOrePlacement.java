26cbba7b0b9bba850c42bf195d7218402be4cef2
WinEpicFin <fia5@aber.ac.uk>
2024-02-13 17:26:49 +0000

    Small changes

--- Extrabiomes - Forge/src/main/java/net/winepicfin/extrabiomes/worldgen/features/ore/ModOrePlacement.java
+++ Extrabiomes - Forge/src/main/java/net/winepicfin/extrabiomes/worldgen/features/ore/ModOrePlacement.java
@@ -1,4 +1,19 @@
 package net.winepicfin.extrabiomes.worldgen.features.ore;
 
+import net.minecraft.world.level.levelgen.placement.*;
+
+import java.util.List;
+
 public class ModOrePlacement {
+    public static List<PlacementModifier> orePlacement(PlacementModifier placementModifier, PlacementModifier placementModifier1) {
+        return List.of(placementModifier, InSquarePlacement.spread(), placementModifier1, BiomeFilter.biome());
+    }
+
+    public static List<PlacementModifier> commonOrePlacement(int pCount, PlacementModifier pHeightRange) {
+        return orePlacement(CountPlacement.of(pCount), pHeightRange);
+    }
+
+    public static List<PlacementModifier> rareOrePlacement(int pChance, PlacementModifier pHeightRange) {
+        return orePlacement(RarityFilter.onAverageOnceEvery(pChance), pHeightRange);
+    }
 }
