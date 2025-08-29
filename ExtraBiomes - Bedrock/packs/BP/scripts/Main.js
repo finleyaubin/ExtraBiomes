1217825f456accb0f07bdd607407696996ab5af7
fia5 <fia5@aber.ac.uk>
2025-08-29 12:50:50 +0100

    fixed nether ores (kinda)

--- ExtraBiomes - Bedrock/packs/BP/scripts/Main.js
+++ ExtraBiomes - Bedrock/packs/BP/scripts/Main.js
@@ -1,4 +1,31 @@
-import { system } from "@minecraft/server";
+import { system, ItemTypes, world } from "@minecraft/server";
+
+/** @type {import("@minecraft/server").BlockCustomComponent} */
+const MushroomGrowComponent = {
+  onPlayerInteract(event) {
+    const { block, player, dimension } = event;
+    const item = player?.getComponent("minecraft:equippable")?.getEquipment("Mainhand");
+
+    if (!item) return;
+
+    // Check if the player is holding bone meal
+    if (item.typeId === "minecraft:bone_meal") {
+      // 75% chance to just consume bone meal with particles
+      if (Math.random() < 0.75) {
+        dimension.spawnParticle("minecraft:crop_growth_emitter", block.center());
+        dimension.playSound("item.bone_meal.use", block.center());
+        item.amount--; // decrement stack
+        return;
+      }
+
+      // 25% chance: grow huge mushroom
+      dimension.runCommand(
+        `structure load extrabiomes:huge_black_mushroom ${block.location.x - 6} ${block.location.y} ${block.location.z - 6}`
+      );
+      item.amount--; 
+    }
+  }
+};
 
 /** @type {import("@minecraft/server").BlockCustomComponent} */
 const XpRewardComponent = {
@@ -9,7 +36,11 @@ const XpRewardComponent = {
   }
 };
 
-system.beforeEvents.startup.subscribe(({ blockComponentRegistry }) => {
+system.beforeEvents.startup.subscribe(({blockComponentRegistry})=>{
+  blockComponentRegistry.registerCustomComponent(
+    "extrabiomes:mushroom_grow",
+    MushroomGrowComponent
+  );
   blockComponentRegistry.registerCustomComponent(
     "extrabiomes:xp_reward_component",
     XpRewardComponent
